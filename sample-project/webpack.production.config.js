a932c9b4e49cb8ec8284061afc542331d41598e9
Lin Chen <gggchenlin@gmail.com>
2019-12-20 23:35:01 -0800

    add hash cache in the filename

--- sample-project/webpack.production.config.js
+++ sample-project/webpack.production.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: __dirname + "/app/main.js",
     output: {
         path: __dirname + "/build",
-        filename: "bundle.js"
+        filename: "bundle-[hash].js"
     },
     devtool: 'none',
     optimization: {
