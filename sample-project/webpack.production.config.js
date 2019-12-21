2da157045d40423670912145fefb0e224e15106b
Lin Chen <gggchenlin@gmail.com>
2019-12-20 23:23:30 -0800

    webpack.production.config.js

---
+++ sample-project/webpack.production.config.js
@@ -0,0 +1,55 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    devtool: 'none',
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                        }
+                    }, {
+                        loader: "postcss-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    
+    plugins: [
+        new webpack.BannerPlugin("Copyright@2019"),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.HotModuleReplacementPlugin(), 
+        new webpack.optimize.OccurrenceOrderPlugin(), 
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css")
+    ]
+};
