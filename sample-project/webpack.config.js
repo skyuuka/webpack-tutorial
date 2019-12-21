c29f9f4b2f5ad3190c4561a1e348605be30d64c9
Lin Chen <gggchenlin@gmail.com>
2019-12-20 22:30:45 -0800

    css as module

--- sample-project/webpack.config.js
+++ sample-project/webpack.config.js
@@ -18,6 +18,20 @@ module.exports = {
                     loader: "babel-loader"
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+                        }
+                    }
+                ]
             }
         ]
     }
